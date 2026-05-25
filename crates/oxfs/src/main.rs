730770293e40c37266ef580eca70eedb73eaab7a
Arjun Kumar <arjunkmrm@gmail.com>
2026-05-25 23:49:45 +0800

    fix: drop writeback FUSE option for kernel compat

--- crates/oxfs/src/main.rs
+++ crates/oxfs/src/main.rs
@@ -69,7 +69,6 @@ fn mount<M: MetaEngine + 'static>(
 
     let mut config = fuser::Config::default();
     config.mount_options.push(fuser::MountOption::FSName("oxfs".into()));
-    config.mount_options.push(fuser::MountOption::CUSTOM("writeback".into()));
     if default_permissions {
         config.mount_options.push(fuser::MountOption::DefaultPermissions);
     }
